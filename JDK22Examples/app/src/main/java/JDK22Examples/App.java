5eb545ab8b4d7a743e112bee9b0cf001dff0d9bd
2334 <2334@EC10261.efacec.pt>
2024-05-02 15:19:52 +0100

    Changed greetings... again!

--- JDK22Examples/app/src/main/java/JDK22Examples/App.java
+++ JDK22Examples/app/src/main/java/JDK22Examples/App.java
@@ -5,7 +5,7 @@ package JDK22Examples;
 
 public class App {
     public String getGreeting() {
-        return "H e l l o    W o r l d!";
+        return "H e l l o    W o r l d!        H e l l o    W o r l d!";
     }
 
     public static void main(String[] args) {
